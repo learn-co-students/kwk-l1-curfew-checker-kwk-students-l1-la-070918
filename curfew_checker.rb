8633ba4f935055ca6a0787a00e54861e4ff3041b
Uma Desai <umadesai38@gmail.com>
2018-07-11 16:05:48 +0000

    Done.

---
+++ curfew_checker.rb
@@ -0,0 +1,11 @@
+def curfew_checker(time)
+  if time == 11
+    "You are in trouble  it is 11!"
+  elsif
+  if time > 11
+    "You are in trouble it is later than 11! "
+  else
+end
+end
+puts curfew_checker(10)
+
